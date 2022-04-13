4bc14fb6b258d429ca45b081d5a402e034a43f89
López, Marcos <Marcos.Lopez@McKesson.com>
2022-04-13 09:47:59 -0600

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
 import * as React from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 
-interface ButtonProps {
+/*interface ButtonProps {
     label: string;
     value: string;
     onClick(e:any): void
-}
-
-interface DisplayProps {
-    label: string;
-}
-
-const DisplayComponent = (props: DisplayProps) => {
-    return(
-        <div>
-            <h1>{props.label}</h1>
-        </div>
-    )
-}
-
-const ButtonComponent = (props:ButtonProps) => {
-    return(
-        <button value={props.value} onClick={props.onClick}>{props.label}</button>
-    );
-}
+}*/
 
 function App() {
 
-    let [buttonClicked, setButtonClicked] = useState('');
+  const handleClick = (event: any) => {
+  };
 
-    const handleClick = (event:any) => {
-        setButtonClicked(event)
-    }
-
-    return (
-        <div>
-            <ButtonComponent label='Click 1' value='1' onClick={handleClick}/>
-            <ButtonComponent label='Click 2' value='2' onClick={handleClick}/>
-            <ButtonComponent label='Click 3!' value='3' onClick={handleClick}/>
-        </div>
-    );
+  return (
+    <div>
+            GOOD BYEEEEE
+    </div>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -33,5 +33,12 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
-    ]
+    ],
+    devServer: {
+        static: path.join(__dirname, 'public'), // boolean | string | array | object, static file location
+        compress: true, // enable gzip compression
+        historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+        hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+        https: false, // true for self-signed, object for cert authority
+    }
 }
